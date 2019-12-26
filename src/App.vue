559fe50983c3938a57a1ff339db73966de013db0
Emanuel Posescu <emanuel.posescu@dacodasoft.ro>
2019-12-26 09:34:53 +0200

    Initial Vue project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app" class="container">
+    <h1>Generate Home Assistant configuration for Livolo RF switches used via Broadlink RM 2/RM Pro/RM Plus remote</h1>
+    <div class="container border">
+      <h2>Step 1 - Add switches</h2>
+      <div class="row">
+        <div class="col-lg-4">
+          <LivoloSwitch index="1"></LivoloSwitch>
+        </div>
+        <div class="col-lg-3">
+          <div class="card">
+            <div class="card-header">
+              Add switch
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Choose a type</h5>
+              <p class="w-100"><a href="#" class="btn btn-primary w-100">One gang</a></p>
+              <p class="w-100"><a href="#" class="btn btn-primary w-100">Two gang</a></p>
+              <p class="w-100"><a href="#" class="btn btn-primary w-100">Three gang</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LivoloSwitch from "./components/LivoloSwitch";
+
+export default {
+  name: 'app',
+  components: {
+    LivoloSwitch
+  }
+}
+</script>
+
---
+++ src/components/LivoloSwitch.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card">
+    <img src="../assets/livolo1.jpg" class="card-img-top" alt="One gang switch" v-if="type == 1">
+    <img src="../assets/livolo2.jpg" class="card-img-top" alt="Two gang switch" v-if="type == 2">
+    <img src="../assets/livolo3.jpg" class="card-img-top" alt="Three gang switch" v-if="type == 3">
+    <div class="card-body">
+      <select class="form-control" v-model="type">
+        <option value="1">One gang</option>
+        <option value="2">Two gang</option>
+        <option value="3">Three gang</option>
+      </select>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon3">Name</span>
+        </div>
+        <input type="text" class="form-control" aria-describedby="basic-addon3" v-model="name">
+      </div>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon3">ID</span>
+        </div>
+        <input type="text" class="form-control-plaintext" aria-describedby="basic-addon3" v-model="id" readonly>
+      </div>
+      <a href="#" class="card-link">Remove</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['index'],
+  name: 'LivoloSwitch',
+  data: function() {
+    return {
+      name: "Livolo Switch " + this.index,
+      type: 2
+    }
+  },
+  computed: {
+    id() {
+      return this.name.toLowerCase().replace(/[^a-z0-9]/g,'_');
+    }
+  }
+}
+</script>
